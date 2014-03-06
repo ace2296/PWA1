608724108b461bba870411ef69a70d6d2d746faa
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-03-06 09:00:54 -0800

    Var setup

---
+++ Wk1/goal1_duel1/js/main.js
@@ -0,0 +1,31 @@
+/*
+Name: Guerra, Andre
+Date: 3/6/14
+Assignment: Goal1: Assignment: Duel1
+*/
+
+//self-executing function
+(function () {
+	console.log('Fight!');
+	
+	//player names
+	var playerOneName = "Batman";
+	var playerTwoName = "Superman";
+	
+	//player damage
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+	
+	//player health
+	var playerOneDamage = 30;
+	var playerTwoDamage = 30;
+	
+	//round
+	
+	
+	function fight() {
+		for(var i = 0; i < 10; i++){
+			
+		}
+	}
+})();
