63682c84f166995eab8d0219df4aeb19bb96f102
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-03-22 09:46:33 -0500

    removed alerts

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -15,53 +15,53 @@ Assignment: Goal 3 - Duel 2
 	//round
 	var round = 0;
 	
-	document.getElementId('#fight_btn').onclick.function fight() {
+	document.getElementId('fight_btn').onclick.function fight() {
 		 
-		/* alert(fighter1[0]+ ":"+ fighter1[1]+ "  *START*  " + fighter2[0] + ":" + fighter2[1]); */
+		document.getElementById('round').setAttribute(fighter1['name']+ ":"+ fighter1['damage']+ "  *START*  " + fighter2['name'] + ":" + fighter2['damage']);
 		
-		for(var i = 0; i < 10; i++){
+		//for(var i = 0; i < 10; i++){
 			 //random formula is - Math.floor(Math.random() * (max - min) + min);
 			 
-			 var minDamage1 = fighter1[2] * .5;
-			 var minDamage2 = fighter2[2] * .5;
-			 var f1 = Math.floor(Math.random()*(fighter1[2] - minDamage1)+minDamage1);
-			 var f2 = Math.floor(Math.random()*(fighter1[2] - minDamage2)+minDamage2);
+			 var minDamage1 = fighter1['health'] * .5;
+			 var minDamage2 = fighter2['health'] * .5;
+			 var f1 = Math.floor(Math.random()*(fighter1['health'] - minDamage1)+minDamage1);
+			 var f2 = Math.floor(Math.random()*(fighter1['health'] - minDamage2)+minDamage2);
 			 
 			 //console.log(f1);
 			 //console.log(f2);
 			 
 			 //Inflict damage
-			 fighter1[1]-=f1;
-			 fighter2[1]-=f2;
+			 fighter1['damage']-=f1;
+			 fighter2['damage']-=f2;
 			 
-			 //console.log(fighter1[1]);
-			 //console.log(fighter2[1]);
+			 //console.log(fighter1['damage']);
+			 //console.log(fighter2['damage']);
 			 
-			 console.log(fighter1[0]+": "+fighter1[1] + " " + fighter2[0]+":"+fighter2[1]);
+			 console.log(fighter1['name']+": "+fighter1['damage'] + " " + fighter2['name']+":"+fighter2['damage']);
 			 
 			 var result = winnerCheck();
 			 console.log(result);
 			 
 			 if (result==="no winner") {
                 round++;
-                alert(fighter1[0]+":"+fighter1[1]+"  *ROUND "+round+" OVER"+"*  "+fighter2[0]+":"+fighter2[1]);
+                document.getElementById('round').setAttribute(fighter1['name']+":"+fighter1['damage']+"  *ROUND "+round+" OVER"+"*  "+fighter2['name']+":"+fighter2['damage']);
 
             } else {
-                alert(result);
+                document.getElementId('round').innerHTML((result));
                 break;
             };
 
 		}
-	};
+	//};
 	
 	function winnerCheck(){
 		 var result="no winner";
-        if (fighter1[1]<1 && fighter2[1]<1) {
+        if (fighter1['damage']<1 && fighter2['damage']<1) {
             result = "You Both Die";
-        } else if(fighter1[1]<1) {
-            result =fighter2[0]+" WINS!!!"
-        } else if (fighter2[1]<1) {
-            result = fighter1[0]+" WINS!!!"
+        } else if(fighter1['damage']<1) {
+            result =fighter2['name']+" WINS!!!"
+        } else if (fighter2['damage']<1) {
+            result = fighter1['name']+" WINS!!!"
         };
        return result;	
 	};
