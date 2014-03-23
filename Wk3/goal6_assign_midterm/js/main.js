6d52614ddbccfec5fab7dca3c88776b678bdc6f5
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-03-23 16:29:12 -0500

    +

--- Wk3/goal6_assign_midterm/js/main.js
+++ Wk3/goal6_assign_midterm/js/main.js
@@ -4,20 +4,25 @@
 (function () {
 
 var button = document.getElementById("info_btn"); 
+var i = 0;
 
-var studentArray = {
-	name: 'Andre Guerra',
-	address: {
-		street: '123 Way Street',
-		city: 'Bend',
-		state: 'OR',
-	},
-	gpa: [4.0,3.5,3.2]
-};
+var studentArray = [
+    {
+        name: 'test1',
+        address: {street: '3300 University', city: 'Winter Park', state: 'Florida'},
+        gpa: [2.5, 3.0, 4.0],
+        date: new Date()
+    },
+    {
+        name: 'test2',
+        address: {street: '123 Test Dr', city: 'Orlando', state: 'Florida'},
+        gpa: [2.0, 3.2, 3.0],
+        date: new Date()
+    }];
 
-console.log('Name: ' + studentArray.name);
-console.log('Address: ' + studentArray.address.street + ', ' + studentArray.address.city + ' ' + studentArray.address.state);
-console.log('GPA: ' + studentArray.gpa);
+//console.log('Name: ' + studentArray.name);
+//console.log('Address: ' + studentArray.address.street + ', ' + studentArray.address.city + ' ' + studentArray.address.state);
+//console.log('GPA: ' + studentArray.gpa);
  
 // JS "Class"
 function Student (theName, theStreet, theCity, theState, theGPA) {
@@ -38,18 +43,22 @@ function Student (theName, theStreet, theCity, theState, theGPA) {
 }
  
 // function to create and add student object to array
-function addStudent (theName, theStreet, theCity, theState, theGPA) {
-  
-  studentArray.push(new Student(theName, theStreet, theCity, theState, theGPA));
-  
-}
+var addStudent = function(newname, newstreet, newcity, newstate, newgpa, newdate){
 
-Student('Bugs Bunny', '123 Bond St', 'Waco', 'TX', [3.0,3.1,2.2]);
-Student('Willy Wonka', '123 Wonka Way', 'Candy', 'NY', [4.0,3.7,3.8]);
+  studentArray.push({
+    name: newname,
+      address: {street: newstreet, city: newcity, state: newstate},
+      gpa: newgpa,
+      date: newdate
+  }); 
+};
+
+addStudent('Bugs Bunny', '123 Bond St', 'Waco', 'TX', [3.0,3.1,2.2], studentArray[i].date);
+addStudent('Willy Wonka', '123 Wonka Way', 'Candy', 'NY', [4.0,3.7,3.8], studentArray[i].date);
 
 //Average GPA function
 
-var gpa = function(arr){
+var gpaAvg = function(arr){
 
         var sum = 0;
 
@@ -65,11 +74,35 @@ var gpa = function(arr){
 button.onclick = next;
 
 function next() {
-	document.querySelector("#name").querySelector("p").innerHTML = 'Name: ' + studentArray.name;
-	document.querySelector("#address").querySelector("p").innerHTML = 'Address: ' + studentArray.address.street + ', ' + studentArray.address.city + ' ' + studentArray.address.state;
-	document.querySelector("#gpa").querySelector("p").innerHTML = 'GPA: ' + studentArray.gpa;
-}
 
+   var innerName = document.querySelector("#name").querySelector("p");
+   var innerAddress = document.querySelector("#address").querySelector("p");
+   var innerGpa = document.querySelector("#gpa").querySelector("p");
+   var innerAvgGpa = document.querySelector("#gpaavg").querySelector("p");
+   var innerDate = document.querySelector("#date").querySelector("p");
+
+   if (i !== max+1){
+    innerName.innerHTML = 'Name:  ' + studentArray[i].name;
+    innerAddress.innerHTML = 'Address:  ' + studentArray[i].address.street + ' ' + studentArray[i].address.city + ' ' + studentArray[i].address.state;
+    innerGpa.innerHTML = 'GPA:  ' + studentArray[i].gpa;
+    innerDate.innerHTML = 'Date:  ' + studentArray[i].date;
+	//innerAvgGpa.innerHTML = 'Average GPA:  ' + avg;
+	
+    //console.log(students[i].gpa);
+  
+    //var avg = gpaAvg(studentArray[i].gpa);
+    
+    // console.log(students[i].gpa.length)
+
+    }else{
+      button.onclick = "return false";    
+      document.querySelector('.buttonred').innerHTML = 'DONE!!!';
+    };
+    i++
+    return false;
+
+}
+var max = studentArray.length;
 
 
 })();
