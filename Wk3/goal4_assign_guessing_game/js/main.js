b8de52f49e33501937a11c7d47cc1d69da6be8a9
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-04-17 15:38:12 -0700

    Set up dom vars and click fn

--- Wk3/goal4_assign_guessing_game/js/main.js
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -10,18 +10,23 @@
 //Game variables
 (function ()
 
-	var playersGuess = 0;
-	var guessesMade = 0;
-	var guessesRemaining = 3;
-	var gameState = 0;
+var playersGuess = 0;
+var guessesMade = 0;
+var guessesRemain = 3;
+var gameState = 0;
 	
-	var dom = {
+var dom = {
 	button: document.querySelector("#button")
     input: document.querySelector("#input"),
-    output: document.querySelector("#ouput"),
-	};
+	output: document.querySelector("#ouput"),
+};
 
+var magicNum = Math.floor(Math.random()* 10 +1);
 
+var clickFn = function(e){
+	console.log(guessesRemain.input.value);
+	validateInput();
+};
 
 
 })();
