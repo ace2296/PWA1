e64e433bd7edc9e2051bf4febb6f7f6a43454378
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-04-17 15:40:42 -0700

    inserted function to validate player's guess

--- Wk3/goal4_assign_guessing_game/js/main.js
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -21,11 +21,28 @@ var dom = {
 	output: document.querySelector("#ouput"),
 };
 
+//Generate random number to guess
 var magicNum = Math.floor(Math.random()* 10 +1);
 
 var clickFn = function(e){
 	console.log(guessesRemain.input.value);
 	validateInput();
+};
+
+// function to validate playersGuess
+var validateInput = function(){
+    playersGuess = parseInt(dom.input.value);
+
+    if(isNaN(playersGuess)){
+        dom.input.innerHTML = "Please enter a number:";
+
+    } else if(playersGuess < 1 || playersGuess > 10){
+        dom.input.innerHTML = "Please enter a number between 1 and 10";
+
+    } else {
+        playGame();
+    }
+
 };
 
 
