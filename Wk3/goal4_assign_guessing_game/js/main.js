f9c25b758669f0d295bd5991f7a45c6751b70e81
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-04-17 15:33:30 -0700

    Added dom vars

--- Wk3/goal4_assign_guessing_game/js/main.js
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -14,5 +14,14 @@
 	var guessesMade = 0;
 	var guessesRemaining = 3;
 	var gameState = 0;
+	
+	var dom = {
+	button: document.querySelector("#button")
+    input: document.querySelector("#input"),
+    output: document.querySelector("#ouput"),
+	};
+
+
+
 
 })();
