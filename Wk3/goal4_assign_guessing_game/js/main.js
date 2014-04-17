3a93c9a812837be7301a71d70cc860e63fe3f7cf
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-04-17 15:32:29 -0700

    Added guessing game files and vars

---
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * GUESSING GAME:
+ *
+ * Created By: Andre Guerra
+ * Date: 04/17/14
+ * 
+ * GUESSING GAME
+ */
+
+//Game variables
+(function ()
+
+	var playersGuess = 0;
+	var guessesMade = 0;
+	var guessesRemaining = 3;
+	var gameState = 0;
+
+})();
