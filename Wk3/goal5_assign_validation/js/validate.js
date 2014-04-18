b4cf7a0f8e0d87bed8d266794b48688e10973d47
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-04-17 18:42:42 -0700

    fixes

--- Wk3/goal5_assign_validation/js/validate.js
+++ Wk3/goal5_assign_validation/js/validate.js
@@ -11,23 +11,18 @@
 
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
+        var username = document.querySelector('#f_username'),
+            email = document.querySelector('#f_email'),
+            phone = document.querySelector('#f_phone'),
+            ssn = document.querySelector('#f_ssn'),
+            password = document.querySelector('#f_password');
         
-        e.name = document.querySelector("#f_username").value;
-        console.log(e.name);
-        e.email = document.querySelector("#f_email");
-        e.phone = document.querySelector("#f_phone");
-        e.password = document.querySelector("#f_password");
-        e.ssn = document.querySelector("#f_ssn");
+        validateField(username);  //id = is the form input field ID
+        validateField(email);
+        validateField(phone);
+        validateField(ssn);
+        validateField(password);
 
-        console.log("Query Selectors Submitted");
-
-        validateField(e.name.value);  //id = is the form input field ID
-        validateField(e.email);
-        validateField(e.phone);
-        validateField(e.password);
-        validateField(e.ssn);
-
-        console.log("Validation Completed");
 
         e.preventDefault();
         return false;
@@ -36,16 +31,25 @@
 
     var validateField = function(inputName){
 
-        if (inputName.name === '#f_username'){
-            var pattern = 'a RegEx pattern goes here'
+        if (inputName.name === 'f_username'){
+          var pattern = /^[A-Z]+(([\'\,\.\- ][A-Z])?[a-zA-Z]*)*$/;
 
             //You will need to create an else-if statement for each input field id.  The
             //      format will be similar to the above IF statement.
 
 
+        }else if (inputName.name === 'f_email'){
+          var pattern = /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/
+        }else if (inputName.name === 'f_phone'){
+          var pattern = /^[\\(]{0,1}([0-9]){3}[\\)]{0,1}[ ]?([^0-1]){1}([0-9]){2}[ ]?[-]?[ ]?([0-9]){4}[ ]*((x){0,1}([0-9]){1,5}){0,1}$/
+        }else if (inputName.name === 'f_ssn'){
+          var pattern = /^(\d{3}-?\d{2}-?\d{4}|XXX-XX-XXXX)$/;
+        }else if (inputName.name === 'f_password'){
+          var pattern = /^[A-Za-z]\w{3,14}$/;
         };
+          
 
-        var pass = 'the RegEx .test statement is needed here';
+        var pass = pattern.test(inputName.value);
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
 
         if (!pass || inputName.value.length < 2){
